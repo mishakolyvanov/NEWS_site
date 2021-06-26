37eea1a858a92f5f08a2e4d815e7dc64e8f10d98
misha <mishakolyvanov@gmail.com>
2021-06-26 13:40:48 +0500

    Created django project

--- news_site/urls.py
+++ news_site/urls.py
@@ -1,4 +1,4 @@
-"""news_site11 URL Configuration
+"""news_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
