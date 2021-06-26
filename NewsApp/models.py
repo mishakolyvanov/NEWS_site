bd2fa2f167e72350fcbdbd88b59a6ff917256aaf
misha <mishakolyvanov@gmail.com>
2021-06-26 17:07:40 +0500

    NewsApp added

---
+++ NewsApp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class News(models.Model):
+    news_title = models.CharField('Заголовок', max_length=200)
+    news_text = models.TextField('Текст новости')
+    pub_date = models.DateTimeField('Время публикации новости')
