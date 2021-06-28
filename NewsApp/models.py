d7ed9d1fb89ca468d1c031e2e57ecbc3a1011ce6
misha <mishakolyvanov@gmail.com>
2021-06-28 16:42:04 +0500

    templates added

--- NewsApp/models.py
+++ NewsApp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 class News(models.Model):
     news_title = models.CharField('Заголовок', max_length=200)
     news_text = models.TextField('Текст новости')
     pub_date = models.DateTimeField('Время публикации новости')
+
+    def __str__(self):
+        return self.news_title
--- NewsApp/views.py
+++ NewsApp/views.py
@@ -1,4 +1,32 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+from NewsApp.models import News
 
 def index(request):
-    return HttpResponse("hi")
+    all_news_list = News.objects.order_by('-pub_date')
+    return render(request, 'list.html', {'all_news_list': all_news_list})
+
+def detail(request, news_id):
+    try:
+        a = News.objects.get( id = news_id)
+    except:
+        raise Http404("Статья не найдена!")
+
+    return render(request, 'detail.html', {'news': a})
+# def index(request):
+#     latest_news_list = News.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.news_title for q in latest_news_list])
+#     return HttpResponse(output)
+
+
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+#
+#
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+#
+#
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
